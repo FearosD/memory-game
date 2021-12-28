6b9d70d1c89c53f4956c83a08e37f33b2a807306
FearosD <fearos@valar.ru>
2021-12-28 17:05:17 +0300

    add func

--- js/script.js
+++ js/script.js
@@ -7,17 +7,62 @@ const flipCard = (event) => {
 
 cards.forEach((card) => card.addEventListener('click', flipCard));
 
-const choiceField = (event) => {
-  const choiceFields = document.getElementsByName('gameField');
-  let gameField;
-  for (let i = 0; i < choiceFields.length; i++) {
-    if (choiceFields[i].checked) {
-      gameField = choiceFields[i].value;
-    }
+// const choiceField = (event) => {
+//   const choiceFields = document.getElementsByName('gameField');
+//   let gameField;
+//   for (let i = 0; i < choiceFields.length; i++) {
+//     if (choiceFields[i].checked) {
+//       gameField = choiceFields[i].value;
+//     }
+//   }
+//   return gameField;
+// };
+
+// const choiceBtn = document.querySelector('.choice__button');
+
+// choiceBtn.addEventListener('click', choiceField);
+
+class MemoryGame {
+  constructor() {
+    this.widthField = 0;
   }
-  return gameField;
-};
 
-const choiceBtn = document.querySelector('.choice__button');
+  choiceField() {
+    const choiceFields = document.getElementsByName('gameField');
+    let gameField;
+    for (let i = 0; i < choiceFields.length; i++) {
+      if (choiceFields[i].checked) {
+        gameField = choiceFields[i].value;
+      }
+    }
+    this.widthField = gameField.slice(0, 1);
+    this.clearChoice();
+    return true;
+  }
 
-choiceBtn.addEventListener('click', choiceField);
+  clearChoice() {
+    const choiceField = document.querySelector('.game-container__choice');
+    choiceField.remove();
+    return true;
+  }
+
+  runChoice() {
+    this.choiceField();
+      // this.clearChoice();
+      return true;
+  }
+
+  choiceBehavior() {
+    const choiceBtn = document.querySelector('.choice__button');
+    choiceBtn.addEventListener('click', this.choiceField);
+  }
+
+  runGame() {
+    this.choiceBehavior();
+  }
+}
+
+
+const game = new MemoryGame;
+
+game.runGame();
